b583a09fa355e9fa18548f8f307788fcb803808e
aviolini <arrigo.violini@gmail.com>
2021-10-29 09:47:22 +0200

    o

--- ft_containers/tests/mainFT.cpp
+++ ft_containers/tests/mainFT.cpp
@@ -1,7 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mainFT.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aviolini <aviolini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/29 09:45:44 by aviolini          #+#    #+#             */
+/*   Updated: 2021/10/29 09:45:49 by aviolini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
 
 #include "../vector.hpp"
 #include "../stack.hpp"
 #include "../map.hpp"
+
 #include "vectorTests.cpp"
 #include "stackTests.cpp"
 #include "mapTests.cpp"
--- ft_containers/tests/mainSTL.cpp
+++ ft_containers/tests/mainSTL.cpp
@@ -6,16 +6,18 @@
 /*   By: aviolini <aviolini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 11:26:14 by aviolini          #+#    #+#             */
-/*   Updated: 2021/10/28 17:30:44 by aviolini         ###   ########.fr       */
+/*   Updated: 2021/10/29 09:45:58 by aviolini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../vectorOR.hpp"
 #include <iostream>
 #include <string>
+
+// #include "../vectorOR.hpp"
 #include <vector>
 #include <stack>
 #include <map>
+
 #include "vectorTests.cpp"
 #include "stackTests.cpp"
 #include "mapTests.cpp"
