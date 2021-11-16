e06b31055810b4735c0809c5d95117881f0ddcb8
aviolini <arrigo.violini@gmail.com>
2021-11-16 12:40:33 +0100

    o

--- ft_containers/tree.hpp
+++ ft_containers/tree.hpp
@@ -6,7 +6,7 @@
 /*   By: aviolini <aviolini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 11:55:32 by aviolini          #+#    #+#             */
-/*   Updated: 2021/11/15 18:24:38 by aviolini         ###   ########.fr       */
+/*   Updated: 2021/11/16 12:40:19 by aviolini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,11 +120,11 @@ class tree
 {
 public:
 	typedef std::allocator<node<Pair> >					Alloc;
-	typedef node<Pair>									value_type;
 	typedef Alloc										allocator_type;
+	typedef typename allocator_type::value_type			value_type;
     typedef typename allocator_type::size_type		    size_type;
-    typedef value_type&								    reference;
-    typedef const value_type&						    const_reference;
+    typedef typename allocator_type::reference			reference;
+    typedef typename allocator_type::const_reference   const_reference;
     typedef typename allocator_type::difference_type	difference_type;
     typedef typename allocator_type::pointer			pointer;
     typedef typename allocator_type::const_pointer		const_pointer;
