32567ff64031c717a582108b089b66ce25746ee3
aviolini <arrigo.violini@gmail.com>
2021-11-17 16:36:43 +0100

    o

--- ft_containers/tests/main.cpp
+++ ft_containers/tests/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aviolini <aviolini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 00:13:14 by arrigo            #+#    #+#             */
-/*   Updated: 2021/11/17 16:24:41 by aviolini         ###   ########.fr       */
+/*   Updated: 2021/11/17 16:36:40 by aviolini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,9 @@ int main(int argc, char** argv) {
 	// vectorTest< ft::vector<int> >();
 	// stackTest< ft::stack<int, ft::vector<int> > > ();
 	// Form *(Intern:: *methods_arr[3])(std::string const & targetForm) const = {&Intern::CreateShrubberyCreationForm,	&Intern::CreateRobotomyRequestForm,	&Intern::CreatePresidentialPardonForm};
-	std::pair<const char, int> (*ptr)(const char, int) = &std::make_pair<const char, int>;
+	ft::pair<const char, int> (*ptr)(const char, int) = &ft::make_pair<const char, int>;
 	// std::cout << ptr('a',1).first << std::endl;
-	mapTest< ft::map<char,int, std::less<char>, std::allocator<std::pair <const char, int > > >, std::pair<const char, int> >(ptr);   ///NON SI PUO' METTERE FT
+	mapTest< ft::map<char,int, std::less<char>, std::allocator<ft::pair <const char, int > > >, ft::pair<const char, int> >(ptr);
 
 
 	return (0);
