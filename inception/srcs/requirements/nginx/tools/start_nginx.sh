05006a8e71f115cadb022d9fad5cc9ef748c2daa
aviolini <arrigo.violini@gmail.com>
2021-12-21 14:13:24 +0100

    ok nginx

--- inception/srcs/requirements/nginx/tools/start_nginx.sh
+++ inception/srcs/requirements/nginx/tools/start_nginx.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
+cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.copy
 mv default /etc/nginx/sites-available/default
+
+cp /var/www/html/index.nginx-debian.html /var/www/html/index.nginx-debian.html.copy
 mv index.nginx-debian.html /var/www/html/index.nginx-debian.html
+
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.copy
+mv nginx.conf /etc/nginx/
+
 chown -R www-data:www-data /var/www/html/
-service nginx start
-/bin/bash
+
+service nginx start								#grazie all'aggiunta di "daemon off;" in nginx.conf nginx funziona in modalità foreground
+												#senza il bisogno di avviare /bin/bash per non far uscire il container 
+# nginx -g 'daemon off;'						# altro parametro giusto senza il bisogno di aggiungere "daemon off;" in nginx.conf
+# nginx											# equivale a "service nginx start"		
+# /bin/bash"									# è stato tolto grazie all'aggiunta del parametro "daemon off"
