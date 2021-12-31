7ec37cd002a006eef06b62a0dbf77400809b8978
aviolini <arrigo.violini@gmail.com>
2021-12-31 02:03:24 +0100

    volume mariadb

--- inception/srcs/requirements/mariadb/tools/start_mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -15,7 +15,7 @@ mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE < wp_dump.sql
 mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE -e "update wp_users set user_login = '$WP_ADMIN', user_pass = MD5('$WP_ADMIN_PASSWORD'), user_nicename = '$WP_ADMIN', user_email = '$WP_ADMIN@test.com', display_name = '$WP_ADMIN' where id = 1;"
 mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE -e "update wp_users set user_login = '$WP_USER', user_pass = MD5('$WP_USER_PASSWORD'), user_nicename = '$WP_USER', user_email = '$WP_USER@test.com', display_name = '$WP_USER' where id = 2;"
 
-
+ln -s /var/lib/mysql/* /mariadb
 
 #https://serverfault.com/questions/634277/installing-configuring-and-running-mariadb-in-foreground-on-centos-7-via-docker
 # mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
