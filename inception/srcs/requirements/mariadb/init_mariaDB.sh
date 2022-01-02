3d876139b2f559f136951e7d8a1c8e0b4f0e0d4e
aviolini <arrigo.violini@gmail.com>
2022-01-02 18:28:24 +0100

    ok mariadb

--- inception/srcs/requirements/mariadb/init_mariaDB.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build . --tag mariadb_image && \
-docker run -itd --rm --name=mariadb_cont mariadb_image && \
-printf "\e[0;32mmariaDB succesfully started\e[0m\n"
--- inception/srcs/requirements/mariadb/tools/start_mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -3,19 +3,28 @@
 service mysql start
 
 mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE;"
+
+############################
 # mysql -u root -e "GRANT USAGE ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
 # mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';"
+############### IL METODO SOPRA E' PIU' SICURO MA NON PERMETTE LA CONNESSIONE DA REMOTO
+############### METTENDO '%' L'UTENTE $MYSQL_USER PUO' ACCEDERE DA QUALSIASI HOST REMOTO AL DATABASE $MYSQL_DATABASE
 mysql -u root -e "GRANT USAGE ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
 mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
 mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
+############################
 
-
+############### RESTORE DEL BACKUP DELLA CONFIGURAZIONE INIZIALE DI WP FATTA IN PRECEDENZA ALL'INTERNO DEL DATABASE $MYSQL_DATABASE
 mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE < wp_dump.sql
+
+############### AGGIORNAMENTO NOME USERS E PASSWORDS
 mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE -e "update wp_users set user_login = '$WP_ADMIN', user_pass = MD5('$WP_ADMIN_PASSWORD'), user_nicename = '$WP_ADMIN', user_email = '$WP_ADMIN@test.com', display_name = '$WP_ADMIN' where id = 1;"
 mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE -e "update wp_users set user_login = '$WP_USER', user_pass = MD5('$WP_USER_PASSWORD'), user_nicename = '$WP_USER', user_email = '$WP_USER@test.com', display_name = '$WP_USER' where id = 2;"
 
+############### COLLEGAMENTO PER CREARE IL VOLUME PERSISTENTE ACCESSIBILE DALL'ESTERNO
 ln -s /var/lib/mysql/* /mariadb
 
+#IMPOSTAZIONI TIPICHE PER UN MIGLIOR LIVELLO DI SICUREZZA
 #https://serverfault.com/questions/634277/installing-configuring-and-running-mariadb-in-foreground-on-centos-7-via-docker
 # mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
 # mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root'"
@@ -24,7 +33,7 @@ ln -s /var/lib/mysql/* /mariadb
 # mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
 # mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES"
 
+############### mysql IN FOREGROUND
 mysqld_safe
-# /bin/bash
 
-#
+############### ALTRE CONFIGURAZIONI: ##ADDING SETTING in 50-server.cnf
