8091aa03158b0c6813ec17f3b3d91abe411302b0
aviolini <arrigo.violini@gmail.com>
2022-01-03 15:09:05 +0100

    volumes

--- inception/srcs/requirements/mariadb/tools/start_mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -2,23 +2,23 @@
 
 service mysql start
 
-mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE;"
+mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE;" &&\
 
 ############################
 # mysql -u root -e "GRANT USAGE ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
 # mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';"
 ############### IL METODO SOPRA E' PIU' SICURO MA NON PERMETTE LA CONNESSIONE DA REMOTO
 ############### METTENDO '%' L'UTENTE $MYSQL_USER PUO' ACCEDERE DA QUALSIASI HOST REMOTO AL DATABASE $MYSQL_DATABASE
-mysql -u root -e "GRANT USAGE ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
-mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
+mysql -u root -e "GRANT USAGE ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';" &&\
+mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" &&\
+mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"&&\
 ############################
 
 ############### RESTORE DEL BACKUP DELLA CONFIGURAZIONE INIZIALE DI WP FATTA IN PRECEDENZA ALL'INTERNO DEL DATABASE $MYSQL_DATABASE
-mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE < wp_dump.sql
+mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE < wp_dump.sql &&\
 
 ############### AGGIORNAMENTO NOME USERS E PASSWORDS
-mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE -e "update wp_users set user_login = '$WP_ADMIN', user_pass = MD5('$WP_ADMIN_PASSWORD'), user_nicename = '$WP_ADMIN', user_email = '$WP_ADMIN@test.com', display_name = '$WP_ADMIN' where id = 1;"
+mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE -e "update wp_users set user_login = '$WP_ADMIN', user_pass = MD5('$WP_ADMIN_PASSWORD'), user_nicename = '$WP_ADMIN', user_email = '$WP_ADMIN@test.com', display_name = '$WP_ADMIN' where id = 1;" &&\
 mysql -u $MYSQL_USER -p$MYSQL_USER_PASSWORD $MYSQL_DATABASE -e "update wp_users set user_login = '$WP_USER', user_pass = MD5('$WP_USER_PASSWORD'), user_nicename = '$WP_USER', user_email = '$WP_USER@test.com', display_name = '$WP_USER' where id = 2;"
 
 # chown -R www-data:www-data /var/lib/mysql/$MYSQL_DATABASE
--- inception/srcs/requirements/php-fpm/tools/start_php.sh
+++ inception/srcs/requirements/php-fpm/tools/start_php.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 #IMPOSTAZIONI WORDPRESS
-./start_wordpress.sh
+# ./start_wordpress.sh
 
 #FILE ESEMPIO info.php
-mv info.php /var/www/html/
-chown www-data:www-data /var/www/html/info.php
+# mv info.php /var/www/html/
+# chown www-data:www-data /var/www/html/info.php
 
-mkdir /run/php		#e' richiesta da php-fpm7.3
+# mkdir /run/php		#e' richiesta da php-fpm7.3
 php-fpm7.3
 
 #NEI FILES php-fpm.conf e www.conf ;ADDING SETTING sono i parametri modificati
