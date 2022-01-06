d4431ad63e0fb2c9a1ff484005adca1e093afcdd
aviolini <arrigo.violini@gmail.com>
2022-01-06 12:29:27 +0100

    ok

--- inception/srcs/requirements/vsftpd/tools/start_vsftpd.sh
+++ inception/srcs/requirements/vsftpd/tools/start_vsftpd.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 #############
 
+useradd -m tin #METTERE LE VARAIBILI AMBIENTE E LA PASSWORD
+mkdir /home/tin/ftp
+chmod a-w /home/tin/ftp
+# chown -R tin:tin /home/tin/
+mkdir /home/tin/ftp/files
+
+echo "test file" >> /home/tin/ftp/files/test.txt
+chown nobody:nogroup -R /home/tin/ftp
+
+echo "tin" >> /etc/vsftpd.userlist
+service vsftpd start
 
 /bin/bash
