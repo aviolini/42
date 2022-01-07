bec56c9d0d30f690a9d1372baa29d75bd65f051a
aviolini <arrigo.violini@gmail.com>
2022-01-07 15:18:04 +0100

    ok vsftpd

--- inception/srcs/requirements/vsftpd/tools/start_vsftpd.sh
+++ inception/srcs/requirements/vsftpd/tools/start_vsftpd.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-#############
-useradd -m -p $(perl -e"print crypt($WP_ADMIN_PASSWORD, aa)") $WP_ADMIN
-echo "$WP_ADMIN" >> /etc/vsftpd.userlist
-service vsftpd start
+############# CREAZIONE UTENTE CON LE CREDENZIALI DELL'UTENTE ADMIN DI WORDPRESS
+useradd -m -g www-data -p $(perl -e"print crypt($WP_ADMIN_PASSWORD, aa)") $WP_ADMIN
 
-/bin/bash
+############# ISCRIZIONE UTENTE ALLA LISTA DI vsftpd
+echo "$WP_ADMIN" >> /etc/vsftpd.userlist
+
+############# ACCESSO GARANTITO ALL'UTENTE E AL GRUPPO www-data
+chmod -R 775 /var/www/html/wordpress
+
+############# AVVIO DEL SERVIZIO
+mkdir -p /var/run/vsftpd/empty
+vsftpd
